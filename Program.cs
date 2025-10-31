3afaa8f239ae5aa9e8479957e6071f137cb4437e
Yosef Triadi <55664947+YosefTriadi17@users.noreply.github.com>
2025-10-31 17:41:54 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-//app.UsePathBase("/welcome");
 app.UseRouting();
-
-//app.Use(async (context, next) =>
-//{
-//   if (!context.Request.Path.StartsWithSegments("/welcome"))
-//    {
-//        context.Response.Redirect("/welcome" + context.Request.Path + context.Request.QueryString);
-//        return;
-//    }
-//    await next();
-//});
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
